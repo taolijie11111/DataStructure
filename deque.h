e951fb30daf41ddb457824be166290370cd317b1
taolijie <taolijie11111@gmail.com>
2022-03-20 12:57:51 +0800

    add

---
+++ deque.h
@@ -0,0 +1,14 @@
+/**
+ * @file deque.h
+ * @author taolijie (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-03-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+//有关+=，+，++，--
+//要注意的点：跨越缓冲区
+
